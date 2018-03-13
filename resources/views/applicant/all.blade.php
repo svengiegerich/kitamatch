71a676a5177cbb4855b017dadea14fc0c2920af8
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-13 13:52:05 +0100

    show all

--- resources/views/applicant/all.blade.php
+++ resources/views/applicant/all.blade.php
@@ -4,8 +4,19 @@
 	<title>Applicant</title>
   </head>
 <body>
-    @foreach($applicants as $applicant)
-        {{$applicant->first_name}}
-    @endforeach
+    <table>
+        <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Adress</th>
+        </tr>
+        @foreach($applicants as $applicant)
+            <tr>
+                <td>{{$applicant->first_name}}</td>
+                <td>{{$applicant->last_name}}</td>
+                <td>{{$applicant->adress_name}}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
