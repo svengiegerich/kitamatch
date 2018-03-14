49b0b65148a9703abbd8bc3ba53c1887d024c6ab
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-14 08:08:16 +0100

    .

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Program;
+use App\Http\Controllers\ProgramController;
 
 use App\Matching;
 use App\Applicant;
