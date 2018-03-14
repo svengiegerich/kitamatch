410c5afe50ce7ec557285d81660df5a924fda7c0
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-14 11:48:14 +0100

    bootstrap & multiple

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>KitaMatch</title>
+
+        <!-- CSS And JavaScript -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+        
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    </body>
+</html>
--- resources/views/matching/all.blade.php
+++ resources/views/matching/all.blade.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html>
-  <head>
-	<title>Matches</title>
-  </head>
-<body>
-    <table>
+
+@extends('layouts.app')
+
+@section('content')
+
+<table>
+    <tr>
+        <th>Applicant</th>
+        <th>College</th>
+        <th>Active</th>
+    </tr>
+    @foreach($matches as $match)
         <tr>
-            <th>Applicant</th>
-            <th>College</th>
-            <th>Active</th>
+            <td>{{$match->aid}}</td>
+            <td>{{$match->pid}}</td>
+            <td>{{$match->active}}</td>
         </tr>
-        @foreach($matches as $match)
-            <tr>
-                <td>{{$match->aid}}</td>
-                <td>{{$match->pid}}</td>
-                <td>{{$match->active}}</td>
-            </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+    @endforeach
+</table>
+
+@endsection
