2696b6dd1955602f94c92b16db2b58ae128bf202
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-14 12:25:49 +0100

    ;

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -27,7 +27,7 @@ class PreferenceController extends Controller
     public function addByApplicant(Request $request, $aid) {
         $preference = new Preference;
         
-        echo "test:"
+        echo "test:";
         echo $aid;
         
         $preference->id_from = $aid;
