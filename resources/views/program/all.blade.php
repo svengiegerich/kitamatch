c515fc8493efcab99e10bcbb073b231059ae1eb7
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-15 09:49:35 +0100

    print_r

--- resources/views/program/all.blade.php
+++ resources/views/program/all.blade.php
@@ -18,6 +18,7 @@
         </tr>
         @foreach($programs as $program)
             <tr>
+                <?php print_r($program); ?>
                 <td>{{$program->pid}}</td>
                 <td>{{$program->name}}</td>
                 <td>{{$program->capacity}}</td>
