a105cd58d160fc5e33d389f6adfebb6c2253cb4b
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-15 09:07:34 +0100

    preference

--- resources/views/preference/showByApplicant.blade.php
+++ resources/views/preference/showByApplicant.blade.php
@@ -5,10 +5,10 @@
 
 <div class="panel-body">
     
-    <h3>Preferences of Applicant <strong><?php $aids = (Array)$preferences; print_r($aids); ?></strong></h3>
+    <h3>Preferences of Applicant <strong><?php foreach ($preferences as $preference) { echo $preference->id_to; break; } ?></strong></h3>
     
     <!-- New Preference Form -->
-    <form action="/preference/applicant/<?php print_r($aids); ?>" method="POST" class="form-horizontal">
+    <form action="/preference/applicant/<?php foreach ($preferences as $preference) { echo $preference->id_to; break; } ?>" method="POST" class="form-horizontal">
         {{ csrf_field() }}
 
         <!-- Preference ID -->
