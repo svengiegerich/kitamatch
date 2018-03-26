d804de0887b8702ed232dc91dffb0892afcfee90
Sven Giegerich <sven.giegerich@mailbox.org>
2018-03-26 15:17:28 +0200

    ..

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.udag.de'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@matchingtools.de'),
+        'name' => env('MAIL_FROM_NAME', 'KitaMatch'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('matchingtools-de-0001'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Ind1cator'),
 
     /*
     |--------------------------------------------------------------------------
