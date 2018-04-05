1813985fb0795dc350fbf6802fda5e1301d5c53f
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-05 13:11:53 +0200

    ..

--- app/Http/Controllers/CriteriumController.php
+++ app/Http/Controllers/CriteriumController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
+use App\Http\Requests;
 use App\Criterium;
 
 class CriteriumController extends Controller
