ca21157a5cd999417139cefde5beffd1c707356b
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-11 10:26:28 +0200

    .

--- resources/views/applicant/edit.blade.php
+++ resources/views/applicant/edit.blade.php
@@ -29,37 +29,8 @@
             </div>
             <div class="form-group row">
                 <label for="birthday"  class="col-sm-2 col-form-label">Birthday</label>
-                <div class="col-sm-10">    
-                    <script>
-                        $(function() {
-                          $('#birthday').datepicker({
-                               prevText: '&#x3c;zurück', prevStatus: '',
-                                prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
-                                nextText: 'Vor&#x3e;', nextStatus: '',
-                                nextJumpText: '&#x3e;&#x3e;', nextJumpStatus: '',
-                                currentText: 'heute', currentStatus: '',
-                                todayText: 'heute', todayStatus: '',
-                                clearText: '-', clearStatus: '',
-                                closeText: 'schließen', closeStatus: '',
-                                monthNames: ['Januar','Februar','März','April','Mai','Juni',
-                                'Juli','August','September','Oktober','November','Dezember'],
-                                monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
-                                'Jul','Aug','Sep','Okt','Nov','Dez'],
-                                dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
-                                dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-                                dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-                              showMonthAfterYear: false,
-                              showOn: 'both',
-                              //buttonImage: 'media/img/calendar.png',
-                              //buttonImageOnly: true,
-                              dateFormat:'d MM, y'
-                            } 
-                          );
-
-                        });
-                    </script>
-                    
-                    <input type="date" class="form-control" name="birthday" id="birthday" placeholder="" value="<?php if ($applicant->birthday) { echo $applicant->birthday->format('d.m.Y'); } ?>">
+                <div class="col-sm-10">                    
+                    <input type="date" class="form-control" name="birthday" id="birthday" placeholder="" value="<?php if ($applicant->birthday) { echo $applicant->birthday->format('Y-m-d'); } ?>">
                 </div>
                 <div class="invalid-feedback">
                       Valid birthday is required.
