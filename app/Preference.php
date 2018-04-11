ddc9aaf481a63ab029ca4fe61a8fa41b52c68c99
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-11 10:42:17 +0200

    ..

--- app/Preference.php
+++ app/Preference.php
@@ -114,7 +114,11 @@ class Preference extends Model
         //tmp: add geocoordinated way
         //sort by birthday on the same level
         //https://github.com/laravel/ideas/issues/11
-        $applicants = $applicants->sortBy('birthday')->sortBy('order');
+        //$applicants = $applicants->sortBy('birthday')->sortBy('order');
+        $applicants = $applicants->sortBy(function($col)
+{
+    return $col;
+})->values()->all();
         return $applicants; 
     }
     
