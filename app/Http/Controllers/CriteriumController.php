3320814de38281c2365b1aa4407cd10194daa5d9
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-11 15:00:53 +0200

    --

--- app/Http/Controllers/CriteriumController.php
+++ app/Http/Controllers/CriteriumController.php
@@ -74,9 +74,9 @@ class CriteriumController extends Controller
         parse_str($request->order, $criteria);
         $test = [];
         $i = 1;
-        foreach ($criteria as $index => $criteriumId) {
-            $criterium = Criterium::find(50);
-            $criterium->rank = $i;
+        foreach ($criteria['item'] as $index => $criteriumId) {
+            $criterium = Criterium::find($criteriumId);
+            $criterium->rank = $index;
             $criterium->save();
             $i = $i + 1;
             $test[] = $criterium;
