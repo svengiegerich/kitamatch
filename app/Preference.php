9d5846f3eafb2e05e08b902c797cf484844de146
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-11 10:45:29 +0200

    ll

--- app/Preference.php
+++ app/Preference.php
@@ -115,9 +115,7 @@ class Preference extends Model
         //sort by birthday on the same level
         //https://github.com/laravel/ideas/issues/11
         //$applicants = $applicants->sortBy('birthday')->sortBy('order');
-        $applicants = $applicants->sortBy(function($col) {
-            return $col;
-        })->values()->all();
+        $applicants = $applicants->sortBy('birthday', 'DESC')->sortBy('order', 'ASC');
         return $applicants; 
     }
     
