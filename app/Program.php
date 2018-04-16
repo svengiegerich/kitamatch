7d5e1940ec13a684ac962f698b031b398258fe91
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-16 15:47:55 +0200

    program order

--- app/Program.php
+++ app/Program.php
@@ -9,38 +9,38 @@ use Illuminate\Support\Facades\DB;
 class Program extends Model
 {
     //
-	
+
     public function getAll() {
-        return (Program::whereIn('status', [12, 13])->get());
+        return (Program::whereIn('status', [12, 13])->orderBy('name', 'asc')->get());
     }
-    
+
     public function isCoordinated($pid) {
         $res = Program::find($pid);
         return $res->coordination;
     }
-    
+
     public function getCoordinated() {
         $programs = Program::where('coordination', '=', 1)->get();
         return $programs;
     }
-    
+
     public function getProviderId($pid) {
         $program = Program::where('pid', '=', $pid)->first();
         return $program->proid;
     }
-    
+
     public function getProgramByUid($uid) {
         $program = Program::where('uid', '=', $uid)->firstOrFail();
         return $program;
     }
-    
+
     public function getProgramsByProid($proid) {
         $programs = Program::where('proid', '=', $proid)->get();
         return $programs;
     }
-    
+
     public $primaryKey = 'pid';
     public $timestamps = false;
-    
-    
+
+
 }
