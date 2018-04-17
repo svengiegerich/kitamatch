6aa606798050ba99f0dece7fa03b906ec3336397
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-17 11:24:38 +0200

    jquery css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     </head>
 
     <body class=".bg-light">
@@ -14,16 +15,16 @@
             <nav class="navbar navbar-default">
                 <!-- Navbar Contents -->
             </nav>
-            
-            <div class="py-5 text-center">   
+
+            <div class="py-5 text-center">
                 <h2>KitaMatch</h2>
                 <p class="lead"></p>
             </div>
 
             @yield('content')
-        
+
         </div>
-        
+
         <br>
         <br>
         <br>
