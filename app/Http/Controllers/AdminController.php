560938f58b50c7410a8bce2b95d1736ad82d304f
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-17 23:58:48 +0200

    db

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
+use Illuminate\Support\Facades\DB;
+
 use App\Applicant;
 use App\Program;
 
