88fa6d1422109375370efcc8ff6e2211db1b946b
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 17:55:26 +0200

    .

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -57,7 +57,7 @@
                         <input type="hidden" name="aid" value="{{$applicant->aid}}">
                         <button>Offer</button>
                     </form>
-                    @else if ($applicant->status == 26)
+                    @elseif ($applicant->status == 26)
                         <button disabled>Matched</button>
                     @endif
                 </td>
