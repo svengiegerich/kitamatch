4e33a1ae8f841dffcd68d136e8e5cf5dc7e062d8
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 21:03:12 +0200

    .

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,6 @@ return [
   //options
   //general, register, coordination, finished
   'stage' => 'general',
-  //'coordination_start_date' => date('Y-m-d H:i:s')
-  'coordination_start_date' => '2018-04-20'
+  'coordination_start_date' => date('Y-m-d H:i:s')
+  //'coordination_start_date' => '2018-04-20'
 ];
