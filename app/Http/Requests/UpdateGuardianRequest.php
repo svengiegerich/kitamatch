b054f481aa6810b044daf25b57b3be4a9942e81a
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 13:17:59 +0200

    .

--- app/Http/Requests/UpdateGuardianRequest.php
+++ app/Http/Requests/UpdateGuardianRequest.php
@@ -33,7 +33,8 @@ class UpdateGuardianRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+          'first_name' => 'required|string|min:2',
+          'last_name' => 'required|string|min:2',
         ];
     }
 }
