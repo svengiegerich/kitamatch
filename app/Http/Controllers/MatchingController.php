097eaf3f81c5966739da821682723945184f7518
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 08:40:49 +0200

    ..

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -58,7 +58,7 @@ class MatchingController extends Controller
         echo "<br><br><br><br><br><br>";
 
         //GuzzleHttp\Client
-		      $client = new Client();
+		     /* $client = new Client();
 		        $response = $client->post('https://api.matchingtools.org/hri/demo?optimum=college-optimal',
             [
 			           'auth' => [
@@ -75,7 +75,7 @@ class MatchingController extends Controller
         $result = json_decode($response->getBody(), true);
         $matchingResult = $result['hri_matching'];
 
-        print_r($result);
+        print_r($result);*/
         /*
         //temp: set active = 0 for all previous entries
         $Matching->resetMatches();
