900aef05f9a3c9eea6b79c85fd2a2dc642aa34df
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 15:40:46 +0200

    ..

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,6 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <div class="g-recaptcha" data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}"></div>
                                 <br>
 
                                 <button type="submit" class="btn btn-primary">
