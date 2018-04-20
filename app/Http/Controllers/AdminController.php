4f5feba2ab599e6876b72f69d4edb689b357a0ad
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-20 11:50:21 +0200

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
         $match->applicant_name = $applicant->last_name . " " . $applicant->first_name;
         $program = Program::where('pid', '=', $match->pid)->first();
         $match->program_name = $program->name;
-        $match->status_tets = Code::where('code', '=', $match->status)->first()->value;;
+        $match->status_text = Code::where('code', '=', $match->status)->first()->value;
         }
       }
       $data = $this->generateDashboard();
