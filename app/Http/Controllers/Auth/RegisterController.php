9bde0cd4741dc9a5311f20c875bb01abab75f9c1
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-26 14:22:05 +0200

    -

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 /**
+*RegisterConroller
+*--------------------------------------------------------------------------
 * This controller handles the registration of new users as well as their
 * validation and creation.
 */
