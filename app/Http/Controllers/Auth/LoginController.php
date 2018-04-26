bc8624f76b21fdf996722d4e0f475a6a4bb4f4dc
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-26 14:31:09 +0200

    .

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,16 +13,16 @@
  | Login Controller
  |--------------------------------------------------------------------------
  */
- /**
- * This controller handles authenticating users for the application and
- * redirecting them to your home screen. The controller uses a trait
- * to conveniently provide its functionality to your applications.
- */
 
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+/**
+* This controller handles authenticating users for the application and
+* redirecting them to your home screen. The controller uses a trait
+* to conveniently provide its functionality to your applications.
+*/
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
