e56ebb7b0d307ac623e75317c251fa6c51dda029
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-26 14:05:13 +0200

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,28 +19,12 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 /**
-*--------------------------------------------------------------------------
-* Register Controller
-*--------------------------------------------------------------------------
-*
+* Register Controller:
 * This controller handles the registration of new users as well as their
-* validation and creation. By default this controller uses a trait to
-* provide this functionality without requiring any additional code.
-*
+* validation and creation.
 */
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
     /**
