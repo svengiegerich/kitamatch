a3376473a024aaf63200f88a3d02712077a64ac8
Sven Giegerich <sven.giegerich@mailbox.org>
2018-04-27 14:17:03 +0200

    documentation

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -39,86 +39,139 @@ class ApplicantController extends Controller
   }
 
   /**
-  * Calls the add applicant view
+  * Call the 'add applicant' view
   *
   * @param integer $gid Guardian-ID
-  * @return view
+  * @return view applicant.add
   */
   public function add($gid) {
     $guardian = Guardian::findOrFail($gid);
     return view('applicant.add', array('guardian' => $guardian));
   }
 
-    public function create(ApplicantRequest $request, $gid) {
-        $request->request->add(['gid' => $gid]);
-        $applicant = $this->store($request);
-        return redirect()->action('PreferenceController@showByApplicant', ['aid' => $applicant->aid]);
-    }
+  /**
+  * Handle the creation of an applicant
+  *
+  * @param App\Http\Requests\ApplicantRequest $request
+  * @param integer $gid Guardian-ID
+  * @return action PreferenceController@showByApplicant
+  */
+  public function create(ApplicantRequest $request, $gid) {
+      $request->request->add(['gid' => $gid]);
+      $applicant = $this->store($request);
+      return redirect()->action('PreferenceController@showByApplicant', ['aid' => $applicant->aid]);
+  }
 
-    public function store(ApplicantRequest $request) {
-        //Validation
-        $applicant = new Applicant;
-        $applicant->gid = $request->gid;
-        $applicant->first_name = $request->firstName;
-        $applicant->last_name = $request->lastName;
-        $applicant->birthday = strtotime($request->birthday);
-        $applicant->gender = $request->gender;
-        $applicant->status = 21;
-        $applicant->save();
+  /**
+  * Store a new applicant
+  *
+  * @param App\Http\Requests\ApplicantRequest $request
+  * @param integer $gid Guardian-ID
+  * @return action PreferenceController@showByApplicant
+  */
+  public function store(ApplicantRequest $request) {
+    $applicant = new Applicant;
+    $applicant->gid = $request->gid;
+    $applicant->first_name = $request->firstName;
+    $applicant->last_name = $request->lastName;
+    $applicant->birthday = strtotime($request->birthday);
+    $applicant->gender = $request->gender;
+    $applicant->status = 21;
+    $applicant->save();
+    return $applicant;
+  }
 
-        return $applicant;
-    }
+  /**
+  * Show a single applicant
+  *
+  * @param integer $aid Applicant-ID
+  * @return view applicant.edit
+  */
+  public function show($aid) {
+    $applicant = Applicant::findOrFail($aid);
+    $guardian = Guardian::find($applicant->gid);
+    $applicant->guardianName = $guardian->last_name . " " . $guardian->first_name;
+    return view('applicant.edit', array('applicant' => $applicant));
+  }
 
-    public function show($aid) {
-        $applicant = Applicant::findOrFail($aid);
-        $guardian = Guardian::find($applicant->gid);
-        $applicant->guardianName = $guardian->last_name . " " . $guardian->first_name;
-        return view('applicant.edit', array('applicant' => $applicant));
-    }
+  /**
+  * Show a all applicants
+  *
+  * @return view applicant.all
+  */
+  public function all() {
+    $applicants = Applicant::all();
+    return view('applicant.all', array('applicants' => $applicants));
+  }
 
-    public function all() {
-        $applicants = Applicant::all();
-        return view('applicant.all', array('applicants' => $applicants));
-    }
+  /**
+  * Call the 'add applicant' view
+  *
+  * @param App\Http\Requests\ApplicantRequest $request
+  * @param integer $aid Applicant-ID
+  * @return view applicant.edit
+  */
+  public function edit(ApplicantRequest $request, $aid) {
+    $request->request->add(['aid' => $aid]);
+    $applicant = $this->update($request);
+    return view('applicant.edit', array('applicant' => $applicant));
+  }
 
-    public function edit(ApplicantRequest $request, $aid) {
-        $request->request->add(['aid' => $aid]);
-        $applicant = $this->update($request);
-        return view('applicant.edit', array('applicant' => $applicant));
-    }
+  /**
+  * Delete an applicant
+  *
+  * @param App\Http\Requests\ApplicantRequest $request
+  * @param integer $aid Applicant-ID
+  * @return action ApplicantController@all
+  */
+  public function delete(ApplicantRequest $request, $aid) {
+    $applicant = applicant::findOrFail($aid);
+    $applicant->delete();
+    return redirect()->action('ApplicantController@all');
+  }
 
-    public function delete(ApplicantRequest $request, $aid) {
-        $applicant = applicant::findOrFail($aid);
-        //temp: set active=0 instead of deleting
-        $applicant->delete();
-        return redirect()->action('ApplicantController@all');
-    }
+  /**
+  * Update an applicant
+  *
+  * @param App\Http\Requests\ApplicantRequest $request
+  * @return App\Applicant
+  */
+  public function update(ApplicantRequest $request) {
+    $applicant = Applicant::findOrFail($request->aid);
+    if ($request->firstName) { $applicant->first_name = $request->firstName; }
+    if ($request->lastName) { $applicant->last_name = $request->lastName; }
+    if ($request->gender) { $applicant->gender = $request->gender; }
+    if ($request->birthday) { $applicant->birthday = strtotime($request->birthday); }
+    if ($request->status) { $applicant->status = $request->status; }
+    $applicant->save();
+    return $applicant;
+  }
 
-    public function update(Request $request) {
-        $applicant = Applicant::findOrFail($request->aid);
-        if ($request->firstName) { $applicant->first_name = $request->firstName; }
-        if ($request->lastName) { $applicant->last_name = $request->lastName; }
-        if ($request->gender) { $applicant->gender = $request->gender; }
-        if ($request->birthday) { $applicant->birthday = strtotime($request->birthday); }
-        if ($request->status) { $applicant->status = $request->status; }
-        $applicant->save();
-        return $applicant;
-    }
+  /**
+  * Update status of an applicant to the final successfull matching status
+  *
+  * @param integer $aid Applicant-ID
+  */
+  public function setFinalMatch($aid) {
+    Applicant::where('aid', '=', $aid)->update(array('status' => '26'));
+  }
 
-    public function setFinalMatch($aid) {
-        $request = new Request();
-        $request->setMethod('POST');
-        $request->request->add(['aid' => $aid,
-                               'status' => 26]);
-        $this->update($request);
-    }
+  /**
+  * Update status of an applicant to verified
+  *
+  * @param integer $aid Applicant-ID
+  */
+  public function setValid($aid) {
+    Applicant::where('aid', '=', $aid)->update(array('status' => '22'));
+  }
 
-    public function setValid($aid) {
-        Applicant::where('aid', '=', $aid)->update(array('status' => '22'));
-    }
-
-    public function setPriority($aid) {
-        Applicant::where('aid', '=', $aid)->update(array('status' => '25'));
-        return redirect()->action('ApplicantController@show', $aid);;
-    }
+  /**
+  * Update status of an applicant to priority for matching (first in line)
+  *
+  * @param integer $aid Applicant-ID
+  */
+  public function setPriority($aid) {
+    Applicant::where('aid', '=', $aid)->update(array('status' => '25'));
+    return redirect()->action('ApplicantController@show', $aid);;
+  }
 }
