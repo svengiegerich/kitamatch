8c3dbfc5ffa894ab7b015c566adb23dd3bd66da0
Sven Giegerich <sven.giegerich@mailbox.org>
2018-05-02 18:53:08 +0200

    -

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Provider::class, function (Faker $faker) {
   return [
     'name' => $faker->company,
     'phone' => $faker->e164PhoneNumber,
-    'address' => $faker->streetName . " " $faker->buildingNumber,
+    'address' => $faker->streetName . " " . $faker->buildingNumber,
     'plz' => $faker->postcode,
     'city' => $faker->city,
     'status' => 61,
