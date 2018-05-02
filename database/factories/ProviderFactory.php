317e50f22677db99cfca35ff066a445286ab839c
Sven Giegerich <sven.giegerich@mailbox.org>
2018-05-02 18:58:28 +0200

    -

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Provider::class, function (Faker $faker) {
     'name' => $faker->company,
     'phone' => $faker->e164PhoneNumber,
     'address' => $faker->streetName . " " . $faker->buildingNumber,
-    'plz' => $faker->postcode,
+    'plz' => $faker->numberBetween($min = 1000, $max = 9000),
     'city' => $faker->city,
     'status' => 61,
     'uid' => factory('App\User')->create()->id,
