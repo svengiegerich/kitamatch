e1351dab582e7b3a822549c8fe19ad485277ded7
Sven Giegerich <sven.giegerich@mailbox.org>
2018-05-02 18:50:24 +0200

    -

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -4,11 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Provider::class, function (Faker $faker) {
   return [
-    'name' => $faker->company(),
-    'phone' => $faker->e164PhoneNumber(),
-    'address' => $faker->streetName() . " " $faker->buildingNumber(),
-    'plz' => $faker->postcode(),
-    'city' => $faker->city(),
+    'name' => $faker->company,
+    'phone' => $faker->e164PhoneNumber,
+    'address' => $faker->streetName . " " $faker->buildingNumber,
+    'plz' => $faker->postcode,
+    'city' => $faker->city,
     'status' => 61,
     'uid' => factory('App\User')->create()->id,
   ];
