cc6616adbdd58ad871804f753b6669546e99c864
Sven Giegerich <sven.giegerich@mailbox.org>
2018-05-08 11:02:23 +0200

    -

--- database/migrations/2018_05_02_162222_create_criteria_table.php
+++ database/migrations/2018_05_02_162222_create_criteria_table.php
@@ -23,6 +23,20 @@ class CreateCriteriaTable extends Migration {
 			$table->boolean('rank');
 			$table->boolean('program')->nullable();
 		});
+
+		//custome KitaMatch!
+		DB::insert(
+			"INSERT INTO `criteria` (`cid`, `created_at`, `updated_at`, `criterium_name`, `criterium_value`, `multiplier`, `p_id`, `rank`, `program`) VALUES
+			(1, NULL, NULL, 'parental_status', 820, 1, -1, 1, NULL),
+			(2, NULL, NULL, 'parental_status', 821, 1, -1, 2, NULL),
+			(3, NULL, NULL, 'parental_status', 822, 1, -1, 3, NULL),
+			(4, NULL, NULL, 'volume_of_employment', 830, 1, -1, 4, NULL),
+			(5, NULL, NULL, 'volume_of_employment', 831, 1, -1, 5, NULL),
+			(6, NULL, NULL, 'volume_of_employment', 832, 1, -1, 6, NULL),
+			(7, NULL, NULL, 'siblings', 841, 1, -1, 7, NULL),(101, NULL, NULL, 'volume_of_employment', 833, 1, -1, 8, NULL),
+			(102, NULL, NULL, 'siblings', 840, 1, -1, 9, NULL),
+			(103, NULL, NULL, 'parental_status', 824, 1, -1, 10, NULL)"
+		);
 	}
 
 
