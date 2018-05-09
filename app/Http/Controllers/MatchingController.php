0e8d1587a0f5f36753cdcf0c8d198f611e5886a3
Sven Giegerich <sven.giegerich@mailbox.org>
2018-05-09 15:39:20 +0200

    -

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -95,7 +95,7 @@ class MatchingController extends Controller
 
     $input = $this->prepareMatching();
 
-    if (!(strlen(json_encode($input))>0)) {
+    if (!(strlen($input)>0)) {
       return redirect()->action('AdminController@index');
     }
     print_r(json_encode($input));
