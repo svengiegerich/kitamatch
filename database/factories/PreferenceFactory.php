5031eda9e8ef4b0b3cea5f7ca540b1595b55bc77
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-06 17:12:36 +0200

    j..

--- database/factories/PreferenceFactory.php
+++ database/factories/PreferenceFactory.php
@@ -2,30 +2,28 @@
 
 use Faker\Generator as Faker;
 
-$autoIncrement = autoIncrement();
 
 //Create only preferences for applicants
 $factory->define(App\Preference::class, function (Faker $faker) {
 
-  $autoIncrement->next();
-  $id_to = (int) $autoIncrement->current();
+  $idFrom = $faker->numberBetween($min = 1, $max = config('kitamatch_config.count_applicants'))
+
+  $i = 0;
+  while(count($preference) > 0 or $i=0) {
+    $programId = $faker->numberBetween($min = 1, $max = config('kitamatch_config.count_programs'));
+    $preference = Preference::where('id_from', $idFrom )
+      ->where('id_to', $programId);
+    $i=1;
+  }
 
   return [
     //sample from the count sample applicants
-    'id_from' => $faker->numberBetween($min = 1, $max = config('kitamatch_config.count_applicants')),
+    'id_from' => $idFrom,
     //sample to sample programs
-    'id_to' => $id_to,
+    'id_to' => $programId,
     'status' => 1,
     'pr_kind' => 1,
     //could compute identical ranks for one applicant, are not sorted like 1,2,3,..; but still works with through the perference order
     'rank' => $faker->numberBetween($min = 1, $max = 20),
   ];
 });
-
-//create 5 preferences
-function autoIncrement()
-{
-    for ($i = 0; $i < 5; $i++) {
-        yield $i;
-    }
-}
