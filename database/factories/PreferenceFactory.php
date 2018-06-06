f3e61a8b3ad96e08300689670dfe4c922fdee969
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-06 16:42:49 +0200

    .

--- database/factories/PreferenceFactory.php
+++ database/factories/PreferenceFactory.php
@@ -2,16 +2,30 @@
 
 use Faker\Generator as Faker;
 
+$autoIncrement = autoIncrement();
+
 //Create only preferences for applicants
 $factory->define(App\Preference::class, function (Faker $faker) {
+
+  $autoIncrement->next();
+  $id_to = (int) $autoIncrement->current();
+
   return [
     //sample from the count sample applicants
     'id_from' => $faker->numberBetween($min = 1, $max = config('kitamatch_config.count_applicants')),
     //sample to sample programs
-    'id_to' => $faker->unique()->numberBetween($min = 1, $max = config('kitamatch_config.count_programs')),
+    'id_to' => $id_to,
     'status' => 1,
     'pr_kind' => 1,
     //could compute identical ranks for one applicant, are not sorted like 1,2,3,..; but still works with through the perference order
     'rank' => $faker->numberBetween($min = 1, $max = 20),
   ];
 });
+
+//create 5 preferences for each
+function autoIncrement()
+{
+    for ($i = 0; $i < 5; $i++) {
+        yield $i;
+    }
+}
