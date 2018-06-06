107e0889e1242fad7fb0648851f7964dc180488f
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-06 17:20:18 +0200

    .

--- database/factories/PreferenceFactory.php
+++ database/factories/PreferenceFactory.php
@@ -13,9 +13,10 @@ $factory->define(App\Preference::class, function (Faker $faker) {
     $programId = $faker->numberBetween($min = 1, $max = config('kitamatch_config.count_programs'));
     $preference = App\Preference::where('id_from', $idFrom )
       ->where('id_to', $programId)->get();
-    if (count($preference) == 0) {
+    /*if (count($preference) == 0) {
       $i = 1;
-    }
+    }*/
+    echo count($preference);
   }
 
   return [
