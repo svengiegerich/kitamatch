8a1d4aabb8b685a2d993adbf1bb889612158dcff
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-08 17:05:14 +0200

    .

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -346,7 +346,7 @@ class PreferenceController extends Controller
     foreach ($applicants['item'] as $index => $preferenceId) {
       $preference = Preference::find($preferenceId);
       //waitlist prefs start with rank >= 2 and not 0
-      $preference->rank = $index+2;
+      $preference->rank = $index;
       $preference->save();
     }
     return response()->json([
