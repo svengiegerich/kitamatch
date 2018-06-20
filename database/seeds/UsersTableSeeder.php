637f4341cbf8295987d2084f8345873c0b79f274
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-20 22:57:24 +0200

    .

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,11 +11,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\User', config('kitamatch_config.count_users'))->create();
-        DB::table('users')->insert([
-          'email' => "m@zew.de",
-          'password' => bcrypt('secret'),
-          'account_type' => 5
-        ]);
+      DB::table('users')->insert([
+        'email' => "m@zew.de",
+        'password' => bcrypt('secret'),
+        'account_type' => 5
+      ]);
+      factory('App\User', config('kitamatch_config.count_users'))->create();
     }
 }
