da71c4b85005d4bd644ae4ec2399c69b5fbeff0d
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-20 13:01:45 +0200

    ,

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -255,7 +255,7 @@
         <tbody>
     @foreach($availableApplicants as $applicant)
     @if (
-      (array_key_exists($applicant->aid, $offers) && $offers[$applicant->aid]['id'] == 1)
+      (array_key_exists($applicant->aid, $offers)
       )
     <tr
           @if (array_key_exists($applicant->aid, $offers))
