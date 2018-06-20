6bcb4e9f915f4f261aed8372720634046a35dbef
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-20 11:57:37 +0200

    lp

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -289,6 +289,7 @@ public function cmp($a, $b)
             }
             $availableApplicants = $availableApplicants->sortBy('rank'); */
 
+dd($offers);
 usort($offers, array($this, "cmp"));
 
       return view('preference.uncoordinated', array('program' => $program,
