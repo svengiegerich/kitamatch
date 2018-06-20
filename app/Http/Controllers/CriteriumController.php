7c6a0519939fd9efebe895244c5470f3d0c25eaf
Sven Giegerich <sven.giegerich@mailbox.org>
2018-06-20 15:20:04 +0200

    .

--- app/Http/Controllers/CriteriumController.php
+++ app/Http/Controllers/CriteriumController.php
@@ -108,7 +108,7 @@ class CriteriumController extends Controller
     $maxIndex = max(array_keys($criteria['item']))+1; //from 0
     foreach ($criteria['item'] as $index => $criteriumId) {
         $criterium = Criterium::find($criteriumId);
-        $criterium->rank = pow(2, ($maxIndex - $index + 1));
+        $criterium->rank = ($maxIndex - $index + 1);
         $criterium->save();
     }
     return response()->json([
