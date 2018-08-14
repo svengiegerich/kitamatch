bfc5ce5a25be5172d1f7b3e56706acb9da17dc75
Sven Giegerich <sven.giegerich@mailbox.org>
2018-08-14 13:08:47 +0200

    deutsch dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
 
       @if ($data['totalCapacity'] == count($matches))
       <div class="alert alert-success" role="alert">
-        <strong>Coordination finished!</strong> All applicants are successfully matched to the programs.
+        <strong>Koordinierung beendet!</strong> Alle Bewerber sind erfoglreich verteilt worden.
       </div>
       @endif
     </div>
@@ -25,40 +25,40 @@
 <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Applicants</h4>
+            <h4 class="my-0 font-weight-normal">Bewerber</h4>
           </div>
           <div class="card-body">
             <h1 class="card-title pricing-card-title">{{$data['applicantsVerified']}} <small class="text-muted">/ {{$data['applicantsCount']}}</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>applicants are verified</li>
+              <li>Bewerber sind verifiziert</li>
             </ul>
-            <a href="{{url('/guardian/all')}}"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Verify applicants</button></a>
+            <a href="{{url('/guardian/all')}}"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Berwerber verifizieren</button></a>
           </div>
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Programs</h4>
+            <h4 class="my-0 font-weight-normal">Kitas</h4>
           </div>
           <div class="card-body">
             <h1 class="card-title pricing-card-title">{{$data['programsInactive']}} <small class="text-muted">/ {{$data['programsCount']}}</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>programs are inactive</li>
+              <li>Kitas sind inaktiv</li>
             </ul>
-            <a href="{{url('/program/all')}}"><button type="button" class="btn btn-lg btn-block btn-outline-primary">See programs</button></a>
+            <a href="{{url('/program/all')}}"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Alle Kitas</button></a>
           </div>
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Matching</h4>
+            <h4 class="my-0 font-weight-normal">Vergabe</h4>
           </div>
           <div class="card-body">
             <h1 class="card-title pricing-card-title"><small class="text-muted">{{$data['applicantsFinal']}} /</small> {{count($matches)}} <small class="text-muted">/ {{$data['totalCapacity']}}</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>applicants are finally matched</li>
-              <li>applicants with open offers</li>
-              <li>total capacity</li>
+              <li>Endgültig zugeordnet</li>
+              <li>Noch im Verfahren</li>
+              <li>Verfügbare Plätze</li>
             </ul>
-            <a href="#matches"><button type="button" class="btn btn-lg btn-block btn-outline-primary">See matches</button></a>
+            <a href="#matches"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Zuteilung ansehen</button></a>
           </div>
         </div>
       </div>
@@ -66,20 +66,20 @@
 <div class="row justify-content-center">
     <div class="col-md-6">
       <br>
-      <a target="_blank" href="{{url('/matching/get')}}"><button class="btn btn-primary btn-lg btn-block">Match!</button></a>
+      <a target="_blank" href="{{url('/matching/get')}}"><button class="btn btn-primary btn-lg btn-block">Veragabe starten</button></a>
       <br>
     </div>
 </div>
 
 <div class="row justify-content-center">
   <div class="col-md-10 my-3 p-3 bg-white rounded box-shadow">
-        <h4>All {{count($matches)}} Matches:</h4>
+        <h4>All {{count($matches)}} Zuordnungen:</h4>
         <br>
         <table class="table" id="matches">
             <thead>
                 <tr>
-                    <th>Program</th>
-                    <th>Applicant</th>
+                    <th>Kita</th>
+                    <th>Bewerber</th>
                     <th>Status</th>
                 </tr>
             </thead>
