0a3b88c25904c4d83cd75ddb1eb2b36e867e27d8
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-04 09:54:51 +0200

    show queue

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -88,7 +88,7 @@
 
 </div></div>
 
-<!--<div class="row justify-content-center">
+<div class="row justify-content-center">
   <div class="col-md-12 my-3 p-3 bg-white rounded box-shadow">
 
     <h3>Warteliste</h3>
@@ -181,7 +181,6 @@
     </table>
 
 </div></div>
--->
 
 <div class="row justify-content-center">
     <div class="col-md-12 my-3 p-3 bg-white rounded box-shadow">
