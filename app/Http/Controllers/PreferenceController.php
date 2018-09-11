384391298484efeb960dc29491a8c21231559e97
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-11 22:42:25 +0200

    pl

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -420,7 +420,7 @@ class PreferenceController extends Controller
       $preference->save();
     }*/
     return response()->json([
-      'success' => $applicants['item']
+      'success' => 1
     ]);
   }
 
