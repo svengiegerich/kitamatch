e9106f1dafdb794bab54a74681ed1e8912c86da9
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-11 16:39:31 +0200

    .

--- resources/views/preference/showByProgram.blade.php
+++ resources/views/preference/showByProgram.blade.php
@@ -12,6 +12,10 @@
 <div class="row justify-content-center">
     <div class="col-md-8 my-3 p-3 bg-white rounded box-shadow">
         <h4>List of preferences</h4>
+
+        <span class="label label-default">Default Label</span>
+<span class="label label-primary">Primary Label</span>
+<span class="label label-success">Success Label</span>
                 <table class="table table-hover">
                     <thead>
                         <tr>
