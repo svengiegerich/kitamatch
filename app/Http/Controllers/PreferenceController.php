4385328b12a34a5a0df716b0ba2bcd78e51bf234
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-11 22:43:23 +0200

    lpü

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -411,8 +411,8 @@ class PreferenceController extends Controller
   }
 
   public function reorderByProgramAjax(Request $request, $pid) {
-    /*preferenceIds = $request->all();
-    parse_str($request->order, $preferences);
+    $preferenceIds = $request->all();
+    /*parse_str($request->order, $preferences);
     foreach ($preferences['item'] as $index => $preferenceId) {
       $preference = Preference::find($preferenceId);
       //waitlist prefs start with rank >= 2 and not 0
