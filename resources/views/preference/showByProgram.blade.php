b1a3e25c77a0a985534f8d3d263f1773bcf990b2
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-12 13:24:08 +0200

    lp

--- resources/views/preference/showByProgram.blade.php
+++ resources/views/preference/showByProgram.blade.php
@@ -134,7 +134,7 @@
                                   <form action="/preference/program/undo/{{$program->pid}}" id="delete_{{$preference->prid}}" method="POST">
                                     {{ csrf_field() }}
                                     <input type="hidden" name="prid" value="{{$preference->prid}}"></input>
-                                    <button form="delete_{{$preference->prid}}" class="ui-icon ui-icon-closethick" style="border:none;"></button>
+                                    <button form="delete_{{$preference->prid}}" class="ui-icon ui-icon-arrowreturnthick-1-n" style="border:none;"></button>
                                   </form>
                                 </td>
                             </tr>
