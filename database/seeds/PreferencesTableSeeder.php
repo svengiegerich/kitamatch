a7cae76f8534ee1483efa8b325f21ef9ed202922
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-18 16:29:01 +0200

    .

--- database/seeds/PreferencesTableSeeder.php
+++ database/seeds/PreferencesTableSeeder.php
@@ -11,7 +11,7 @@ class PreferencesTableSeeder extends Seeder
   */
   public function run() {
     //create 0.4 times all sample programs
-    foreach (range(1, config('kitamatch_config.count_programs') * (0.4) * config('kitamatch_config.count_applicants')) as $i) {
+    foreach (range(1, config('kitamatch_config.count_programs') * (0.25) * config('kitamatch_config.count_applicants')) as $i) {
       factory('App\Preference', 1)->create();
     }
     //factory('App\Preference', config('kitamatch_config.count_programs') * (0.25) * config('kitamatch_config.count_applicants'))->create();
