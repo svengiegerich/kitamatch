b882f38fed4b14886a4cf74bff656acf11656aae
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-18 19:17:00 +0200

    .

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       factory('App\User', config('kitamatch_config.count_users'))->create();
+
+      //create super / admin user
+      $user = new App\User();
+      $user->password = Hash::make('secret');
+      $user->email = 'm@zew.de';
+      $user->account_type = "5";
+      $user->save();
     }
 }
