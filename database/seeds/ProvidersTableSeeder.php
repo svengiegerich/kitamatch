1cea72fda91e60b577f1aa8dd060a2e1f471758d
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-18 19:21:53 +0200

    .

--- database/seeds/ProvidersTableSeeder.php
+++ database/seeds/ProvidersTableSeeder.php
@@ -11,6 +11,17 @@ class ProvidersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Provider', config('kitamatch_config.count_providers'))->create();
+      //Random creation
+      //factory('App\Provider', config('kitamatch_config.count_providers'))->create();
+
+      //Sample data
+      $provider = new App\Provider();
+      $provider->name = 'Stadt';
+      $provider->phone = '+4915776638369';
+      $provider->plz = "69115";
+      $provider->city = "Heidelberg";
+      $provider->address = "Bismarckplatz 10";
+      $provider->status = 61;
+      $provider->uid = factory('App\User')->create()->id;
     }
 }
