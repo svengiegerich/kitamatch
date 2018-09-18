005150b0c139a91c6cade4c8c5a2ddb59a6768d0
Sven Giegerich <sven.giegerich@mailbox.org>
2018-09-18 19:36:02 +0200

    .

--- database/seeds/ProgramsTableSeeder.php
+++ database/seeds/ProgramsTableSeeder.php
@@ -15,13 +15,61 @@ class ProgramsTableSeeder extends Seeder
       factory('App\Program', config('kitamatch_config.count_programs'))->create();
 
       //-- Example sample (make sure count is same as in config!)
-      /*$program = new App\Program();
-      $program->name = "Sonnenschein";
+      $program = new App\Program();
+      $program->name = "Sonnenschein - U3";
       $program->capacity = 5;
       $program->p_kind = 2;
       $program->coordiantion = 1;
       $program->status = 12;
-      $program->uid =
-      $program->save();*/
+      $program->uid = factory('App\User')->create(['email' => "sonnenschein.u3@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->proid = 2;
+      $program->save();
+
+      $program = new App\Program();
+      $program->name = "Sonnenschein - Ü3";
+      $program->capacity = 5;
+      $program->p_kind = 2;
+      $program->coordiantion = 1;
+      $program->status = 12;
+      $program->uid = factory('App\User')->create(['email' => "sonnenschein.ue3@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->proid = 2;
+      $program->save();
+
+      $program = new App\Program();
+      $program->name = "DRK Burg Funkelstein";
+      $program->capacity = 7;
+      $program->p_kind = 2;
+      $program->coordiantion = 1;
+      $program->status = 12;
+      $program->uid = factory('App\User')->create(['email' => "funkelstein@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->save();
+
+      $program = new App\Program();
+      $program->name = "St. Marien";
+      $program->capacity = 7;
+      $program->p_kind = 2;
+      $program->coordiantion = 1;
+      $program->status = 12;
+      $program->uid = factory('App\User')->create(['email' => "marien@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->save();
+
+      $program = new App\Program();
+      $program->name = "Emilia";
+      $program->capacity = 5;
+      $program->p_kind = 2;
+      $program->coordiantion = 1;
+      $program->status = 12;
+      $program->uid = factory('App\User')->create(['email' => "emilia@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->save();
+
+      $program = new App\Program();
+      $program->name = "Kaleidoskop";
+      $program->capacity = 8;
+      $program->p_kind = 2;
+      $program->coordiantion = 1;
+      $program->status = 12;
+      $program->uid = factory('App\User')->create(['email' => "kaleidoskop@zew.de", 'password' => Hash::make('secret'),])->id;
+      $program->proid = 1;
+      $program->save();
     }
 }
--- database/seeds/ProvidersTableSeeder.php
+++ database/seeds/ProvidersTableSeeder.php
@@ -22,7 +22,7 @@ class ProvidersTableSeeder extends Seeder
       $provider->city = "Heidelberg";
       $provider->address = "Bismarckplatz 10";
       $provider->status = 61;
-      $provider->uid = factory('App\User')->create()->id;
+      $provider->uid = factory('App\User')->create(['email' => "stadt@zew.de", 'password' => Hash::make('secret'),])->id;
       $provider->save();
 
       $provider = new App\Provider();
@@ -32,7 +32,7 @@ class ProvidersTableSeeder extends Seeder
       $provider->city = "Heidelberg";
       $provider->address = "Bismarckplatz 10";
       $provider->status = 61;
-      $provider->uid = factory('App\User')->create(['email' => "test@zew.de",])->id;
+      $provider->uid = factory('App\User')->create(['email' => "sonnenschein.t@zew.de", 'password' => Hash::make('secret'),])->id;
       $provider->save();
     }
 }
