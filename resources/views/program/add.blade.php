e4a387e494635344bc618ef4ef6a97f850c56f07
Sven Giegerich <sven.giegerich@mailbox.org>
2018-12-27 10:53:53 +0100

    ..

--- resources/views/program/add.blade.php
+++ resources/views/program/add.blade.php
@@ -50,9 +50,9 @@
                 <div class="col-sm-3">
                     <!--<input type="checkbox" class="form-control" name="type" id="type">-->
                     <select class="form-control">
-                      <option name="--" selected>---</option>
-                      <option name="0">Ja</option>
-                      <option name="1">Nein</option>
+                      <option value="---" selected>---</option>
+                      <option value="0">Ja</option>
+                      <option value="1">Nein</option>
                     </select>
                 </div>
             </div>
