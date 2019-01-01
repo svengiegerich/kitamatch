23d785cf3bb0ffb986eb532926e6fb732fc7f201
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-01 21:29:04 +0100

    .

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -115,7 +115,7 @@ class PreferenceController extends Controller
     foreach ($programs as $program) {
       if (!($preferences->contains('id_to', $program->pid))) {
         $provider = Provider::find($program->proid);
-        $select[$program->pid] = $provider->name . " (" . $program->name . ")";
+        $select[$program->pid] = $program->name . " - " . $provider->name;
       }
     }
     asort($select);
