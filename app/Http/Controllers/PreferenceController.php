27a98f5826a60411269f29447010974a6afd1286
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-02 17:54:19 +0100

    .

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -257,7 +257,7 @@ class PreferenceController extends Controller
       $Program = new Program();
       $Matching = new Matching();
       $Provider = new Provider();
-      $lastMatch = new DateTime( '2010-01-31' );
+      $lastMatch = DateTime::createFromFormat('m-d-Y', '10-16-2003')->format('Y-m-d');
       $preferences = $this->getPreferencesUncoordinatedByProgram($pid);
       $providerId = $Program->getProviderId($pid);
       if ($providerId) {
