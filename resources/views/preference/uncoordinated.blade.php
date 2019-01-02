c937cd6a4673c198455acf2544a5cdd924c08c42
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-02 18:30:54 +0100

    .

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -85,7 +85,7 @@
                         id="delete_{{$offers[$applicant->aid]['id']}}" name="delete_{{$offers[$applicant->aid]['id']}}" method="POST">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
-                        <button form="delete_{{$offers[$applicant->aid]['id']}}" type="submit" class="btn btn-secondary">Angebot zurücknehmen</button>
+                        <button form="delete_{{$offers[$applicant->aid]['id']}}" type="submit" class="btn btn-secondary btn-small">Angebot zurücknehmen</button>
                       </form>
                     @endif
                   </td>
