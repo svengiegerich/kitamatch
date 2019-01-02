aa29bebe737d8148d2aeb3f6d0ad947e45d93418
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-02 18:00:52 +0100

    .

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -83,7 +83,7 @@
                       <form action="{{url('/preference/program/uncoordinated/' . $offers[$applicant->aid]['id'])}}" method="POST">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
-                        <button>Zurücknehmen</button>
+                        <button type="button" class="btn btn-secondary btn-sm">Angebot zurücknehmen</button>
                       </form>
                     @endif
                   </td>
