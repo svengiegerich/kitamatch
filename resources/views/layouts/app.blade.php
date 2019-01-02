bb84461e1a9e22c86d077b95cdc4c95607a3d202
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-02 18:25:33 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,18 +15,18 @@
 
     <body class=".bg-light">
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Kitamatch - Kitaplatzvergabe</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li></li>
-      </ul>
-    </div>
-  </nav>
+        <a class="navbar-brand" href="#">Kitamatch - Kitaplatzvergabe</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li></li>
+          </ul>
+        </div>
+      </nav>
 
-        <div class="container">
+        <div class="container pt-10">
 
             @yield('content')
 
