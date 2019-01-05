6d16ee7c0141393780b1bce7b129076e2362e9dc
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-05 11:35:40 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <body class=".bg-light">
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Kitamatch - Kitaplatzvergabe</a>
+        <a class="navbar-brand" href="#">{{config('app.name')}}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
