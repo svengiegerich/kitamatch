af28a877e8fdac178025bdba333fa714629685b3
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-07 00:33:56 +0100

    .

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -73,7 +73,7 @@
 
 <div class="row justify-content-center">
   <div class="col-md-8">
-    <h4>{{count($matches)}} Zuordnungen | {{$data['rounds']}} Koordinierungsrunden</h4>
+    <h4>{{count($matches)}} Zuordnungen | {{$data['countRounds']}} Koordinierungsrunden</h4>
   </div>
 </div>
 
