a86b5aa7802e840929405a16ec8034019b7df668
Sven Giegerich <sven.giegerich@mailbox.org>
2019-01-07 00:29:28 +0100

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -94,7 +94,7 @@ class AdminController extends Controller
     $data['providersCount'] = count($providers);
     $capacitySql = "SELECT SUM(capacity) AS 'totalCapacity' FROM programs";
     $data['totalCapacity'] = DB::select($capacitySql)['0']->totalCapacity;
-    $data['round'] = DB::table('matches')->select(DB::raw('count(DISTINCT  created_at)'))->get();
+    $data['round'] = DB::table('matches')->select(DB::raw('count(DISTINCT  created_at)'))->first();
     print_r($data['round']);
     return $data;
   }
