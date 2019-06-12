12e027c32e1067cd4234c580b46d468a5cc53d98
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-12 15:53:24 +0200

    .

--- resources/views/applicant/edit.blade.php
+++ resources/views/applicant/edit.blade.php
@@ -75,7 +75,7 @@
 @foreach ($criteria_names as $criterium_name)
 <?php
   $criterium_values = $criteria_values->where('criterium_name', '=', $criterium_name);
-  $criterium_list = $criterium_values::pluck('criterium_value', 'criterium_value_description')->toArray();
+  $criterium_list = $criterium_values::pluck('criterium_value', 'criterium_value_description')->all();
 ?>
 
 <div class="form-group row">
