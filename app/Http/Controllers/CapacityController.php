00c7019bb77bb2180ef33a9e3e5f59770bf93ad7
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-19 12:41:13 +0200

    ..

--- app/Http/Controllers/CapacityController.php
+++ app/Http/Controllers/CapacityController.php
@@ -52,7 +52,6 @@ class CapacityController extends Controller
       foreach(config('kitamatch_config.care_starts') as $key_scope => $care_scope) {
         if ($key_start != 0 and $key_scope != 0) {
           $request = new Request();
-          $request->setMethod('POST');
           $request->request->add([
             'pid' => $pid,
             'care_start' => $key_start,
