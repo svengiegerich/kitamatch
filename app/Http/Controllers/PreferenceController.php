042f23f63a000b95ede87c4633e77d1ae6cd375c
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-19 13:42:02 +0200

    -

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -202,8 +202,7 @@ class PreferenceController extends Controller
   // write applicant's preferences based on the feasible set, by care start & scope
   public function setPreferencesByApplicant($aid) {
     $applicant = Applicant::find($aid);
-    print_r($aid);
-    print('<br>');
+    print_r($applicant);
     $feasible_set = Preference::where('pr_kind', '=', 0)->where('id_from', '=', $aid)->where('status', '=', 1)->orderBy('rank')->get();
 
     $i = 1;
