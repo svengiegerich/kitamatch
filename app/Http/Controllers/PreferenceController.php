dd2af96214f66d8f0da45f0b5a317e1c5cf9e4a4
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-26 15:11:50 +0200

    .

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -453,7 +453,7 @@ class PreferenceController extends Controller
                                                     'matches' => $matches,
                                                     'deletedPreferences' => $deletedPreferences));
     } else {
-      $this->showByUncoordinatedProgram($pid);
+      return redirect()->action('PreferenceController@showByUncoordinatedProgram', [$pid]);
     }
   }
 
@@ -531,7 +531,6 @@ class PreferenceController extends Controller
             $offers[$applicant->aid]['status'] = $preference->status;
             $offers[$applicant->aid]['updated_at'] = $preference->updated_at;
             $offers[$applicant->aid]['preferences'] = $this->getPreferencesByApplicant($applicant->aid);
-            print("he");
             if ($applicant->status == 26) {
               $offers[$applicant->aid]['final'] = 1;
             } else {
