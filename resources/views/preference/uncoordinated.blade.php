9b0953de8c7f6dc4d64359681cd29f939f8feceb
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-26 15:47:39 +0200

    .

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -291,7 +291,23 @@
 @foreach (config('kitamatch_config.care_scopes') as $key_scope => $scope)
 @if ($key_scope != -1)
 @if ($program->openOffers[$key_start][$key_scope] < $capacities->where('care_start', '=', $key_start)->where('care_scope', '=', $key_scope)->first()->capacity))
-<?php print_r($servicesApplicants[$applicant->aid]); ?>
+
+@if (isset($servicesApplicants[$applicant->aid][$key_start][$key_scope]))
+  <div class="col-md-6">
+    <form action="{{url('/preference/program/uncoordinated/offer/' . $program->pid)}}" method="POST">
+        {{ csrf_field() }}
+        <input type="hidden" name="aid" value="{{$applicant->aid}}">
+        <input type="hidden" name="sid" value="{{$program->pid}}_{{$key_start}}_{{$key_scope}}">
+        <button class="btn btn-primary">{{$start}}, {{$scope}}</button>
+    </form>
+  </div>
+@else
+<div class="col-md-6">
+  <button class="btn btn-primary" disabled>{{$start}}, {{$scope}}</button>
+</div>
+@endif
+
+
 @endif
 @endif
 @endforeach
