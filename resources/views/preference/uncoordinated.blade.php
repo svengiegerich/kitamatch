6b6dce7bc55d5567171a233927adceb2cd4ff538
Sven Giegerich <sven.giegerich@mailbox.org>
2019-06-26 11:50:18 +0200

    .

--- resources/views/preference/uncoordinated.blade.php
+++ resources/views/preference/uncoordinated.blade.php
@@ -247,11 +247,41 @@
                     @endif
                 </td>
                 <td>
-                    <form action="{{url('/preference/program/uncoordinated/waitlist/' . $program->pid)}}" method="POST">
+                    <!--<form action="{{url('/preference/program/uncoordinated/waitlist/' . $program->pid)}}" method="POST">
                       {{ csrf_field() }}
                       <input type="hidden" name="aid" value="{{$applicant->aid}}">
                       <button class="btn btn-secondary" disabled>Warteliste</button>
-                    </form>
+                    </form>-->
+
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                      Launch
+                    </button>
+
+                    <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+<div class="modal-body">
+  <form action="{{url('/preference/program/uncoordinated/waitlist/' . $program->pid)}}" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="aid" value="{{$applicant->aid}}">
+    <button class="btn btn-secondary" disabled>Warteliste</button>
+  </form>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+</div>
+</div>
+</div>
+</div>
+
                 </td>
               </tr>
           @endif
